8ab9d19765ca60366615203021192dbf6a18c0e1
antonny melo <antonny.mello@gmail.com>
2023-12-14 15:07:28 -0300

    feat: add methods to build a list message

--- src/main/java/module/Main.java
+++ src/main/java/module/Main.java
@@ -1,11 +1,17 @@
 package module;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import it.auties.whatsapp.api.QrHandler;
 import it.auties.whatsapp.api.Whatsapp;
+import it.auties.whatsapp.model.button.misc.ButtonRow;
+import it.auties.whatsapp.model.button.misc.ButtonSection;
 import it.auties.whatsapp.model.contact.ContactJid;
 import it.auties.whatsapp.model.contact.ContactJidProvider;
+import it.auties.whatsapp.model.message.button.ListMessage;
+import it.auties.whatsapp.model.message.button.ListMessage.Type;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,16 +26,60 @@ public class Main {
                 .unregistered(QrHandler.toFile(Paths.get("qr.png"), QrHandler.ToFileConsumer.toTerminal()))
                 .connect().join();
 
+        System.out.println("WhatsApp API initialized!");
+        System.out.println("Waiting to send message to establish connection...");
+
         try {
-            System.out.println("WhatsApp API initialized!");
-            ContactJidProvider contactJidProvider = ContactJid.of("+558599685687").toJid();
-            System.out.println("Waiting to send message to " + contactJidProvider);
             Thread.sleep(30000);
-            System.out.println("WhatsApp API READY TO SEND MESSAGE!");
-            app.sendMessage(contactJidProvider, "Hello World!");
-        } catch (InterruptedException e) {
+            List<String> contacts = getContacts();
+            System.err.println("Sending message to " + contacts.size() + " contacts");
+
+            for (String contact : contacts) {
+                System.out.println("Sending message to " + contact);
+                ContactJidProvider contactJidProvider = ContactJid.of(contact).toJid();
+                ListMessage listMessage = createOptionsList();
+                app.sendMessage(contactJidProvider, listMessage);
+                System.err.println("Waiting 2 seconds to send message to next contact");
+                Thread.sleep(2000);
+            }
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
+
+    private static ListMessage createOptionsList() {
+        List<ButtonRow> buttons = createOptions();
+        ButtonSection section = createSection("Options", buttons);
+        ListMessage listMessage = ListMessage.builder()
+                .sections(List.of(section))
+                .title("Opções")
+                .description("Escolha uma das opções abaixo")
+                .listType(Type.SINGLE_SELECT)
+                .build();
+        return listMessage;
+    }
+
+    private static List<String> getContacts() {
+        List<String> contacts = new ArrayList<>();
+
+        return contacts;
+    }
+
+    private static List<ButtonRow> createOptions() {
+        List<ButtonRow> buttons = new ArrayList<>();
+        buttons.add(ButtonRow.of("First option", "A nice description"));
+        buttons.add(ButtonRow.of("Second option", "A nice description"));
+        buttons.add(ButtonRow.of("Third option", "A nice description"));
+        buttons.add(ButtonRow.of("Fourth option", "A nice description"));
+        buttons.add(ButtonRow.of("Fifth option", "A nice description"));
+        return buttons;
+    }
+
+    private static ButtonSection createSection(String title, List<ButtonRow> buttons) {
+        ButtonSection section = ButtonSection.of(title, buttons);
+        return section;
+    }
+
 }

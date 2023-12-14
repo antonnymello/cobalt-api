fa02ae4e40961ab1db6049c535630f9bad820cf1
antonny melo <antonny.mello@gmail.com>
2023-12-14 12:22:28 -0300

    feat: initial commit

---
+++ src/main/java/module/Main.java
@@ -0,0 +1,23 @@
+package module;
+
+import java.nio.file.Paths;
+
+import it.auties.whatsapp.api.QrHandler;
+import it.auties.whatsapp.api.TextPreviewSetting;
+import it.auties.whatsapp.api.WebHistoryLength;
+import it.auties.whatsapp.api.Whatsapp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Initializing WhatsApp API...");
+
+        Whatsapp.webBuilder()
+                .newConnection()
+                .name("WhatsappAPI")
+                .autodetectListeners(true)
+                .textPreviewSetting(TextPreviewSetting.ENABLED)
+                .checkPatchMacks(true)
+                .unregistered(QrHandler.toTerminal())
+                .connectAwaitingLogout().join();
+    }
+}
